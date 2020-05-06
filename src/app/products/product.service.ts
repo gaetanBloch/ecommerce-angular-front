0cba5c80cdc1f3ce44c558845a00ba3cf9c86df1
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-06 14:15:41 +0200

    Created the get Product List method in the Product Service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from './product.model';
+import { map } from 'rxjs/operators';
+
+interface GetResponse {
+  _embedded: {
+    products: Product[]
+  };
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getProductList(): Observable<Product[]> {
+    return this.http.get<GetResponse>('/api/products')
+      .pipe(
+        map(result => result._embedded.products)
+      );
+  }
 }
