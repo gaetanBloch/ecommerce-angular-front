51021519d0c391faae456b267c8cbc85e0abea8c
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-07 17:38:24 +0200

    Added route for Product Details Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
 
 const routes: Routes = [
   {path: 'search/:keyword', component: ProductListComponent},
   {path: 'categories/:id', component: ProductListComponent},
   {path: 'categories', component: ProductListComponent},
+  {path: 'products/:id', component: ProductDetailsComponent},
   {path: 'products', component: ProductListComponent},
   {path: '', redirectTo: '/products', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
