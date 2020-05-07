9abd0c92e036bf2a182fe5513d2f2fa84945df82
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-07 10:17:23 +0200

    Created Product Category Model

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Product } from './product.model';
 import { map } from 'rxjs/operators';
+
+import { Product } from './product.model';
 
 interface GetResponse {
   _embedded: {
