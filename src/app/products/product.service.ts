926983b3127d7966fad2427d73bf91fba1131dff
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-07 11:00:13 +0200

    Add the method to get the Product Categories in the Product Service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -4,10 +4,17 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Product } from './models/product.model';
+import { ProductCategory } from './models/product-category.model';
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[]
+  };
+}
+
+interface GetResponseProductCategories {
+  _embedded: {
+    productCategories: ProductCategory[]
   };
 }
 
@@ -20,9 +27,18 @@ export class ProductService {
   }
 
   getProductList(categoryId: number): Observable<Product[]> {
-    return this.http.get<GetResponse>(`/api/products/search/findByCategoryId?id=${categoryId}`)
+    return this.http
+      .get<GetResponseProducts>(`/api/products/search/findByCategoryId?id=${categoryId}`)
       .pipe(
         map(result => result._embedded.products)
+      );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.http
+      .get<GetResponseProductCategories>(`/api/product-categories`)
+      .pipe(
+        map(result => result._embedded.productCategories)
       );
   }
 }
