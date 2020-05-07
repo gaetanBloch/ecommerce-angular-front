fc603529db5db098f6e50761882d6d28d339e261
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-07 17:56:19 +0200

    Access the Product Service to get the product from the id

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Product } from '../models/product.model';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent implements OnInit, OnDestroy {
+  private readonly ID = 'id';
+  product: Product;
+  private paramsSubscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private productService: ProductService, private route: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    this.paramsSubscription = this.route.paramMap.subscribe(params => {
+      this.handleProductDetails(params);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.paramsSubscription) {
+      this.paramsSubscription.unsubscribe();
+    }
+  }
+
+  private handleProductDetails(params: ParamMap) {
+    this.productService.getProduct(params.get(this.ID)).subscribe(product => {
+      this.product = product;
+    });
+  }
 }
